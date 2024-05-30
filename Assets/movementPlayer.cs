9aae7a9ecd348c3ccd4d64ac95c06406aab634eb
Karsten <karstenseijkens@gmail.com>
2024-05-30 08:54:42 +0200

    added movement player and started on enemy ai

--- Assets/movementPlayer.cs
+++ Assets/movementPlayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movmentPlayer : MonoBehaviour
+public class movementPlayer : MonoBehaviour
 {
     private Rigidbody rb;
     private float inputDirX;
