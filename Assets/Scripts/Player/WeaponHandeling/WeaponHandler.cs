fb1c277c90591d8a705f5a4dcd61259b95d40de2
Karsten <karstenseijkens@gmail.com>
2024-12-09 18:14:28 +0100

    added weapon handler

---
+++ Assets/Scripts/Player/WeaponHandeling/WeaponHandler.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class WeaponHandler : MonoBehaviour
+{
+    [System.Serializable]
+    public struct WeaponModel {//change because vars only need to be get after being set
+        public GameObject Model;
+        public int Id;
+        public float XOffset;
+        public float YOffset;
+        
+        public float ZOffset;
+    }
+
+    [SerializeField]
+    public WeaponModel[] weaponModels;
+    public GameObject spawnedWeapon;
+    void Start()
+    {
+        SetWeapon(1);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SetWeapon(int id)
+    {
+        foreach(Transform child in transform){
+            GameObject.Destroy(child.gameObject);
+        }
+        WeaponModel weaponModel = Array.Find(weaponModels , m => m.Id == id);//add check
+        spawnedWeapon = Instantiate(weaponModel.Model);
+        spawnedWeapon.transform.SetParent(this.transform);
+        spawnedWeapon.transform.localPosition = new Vector3(weaponModel.XOffset, weaponModel.YOffset, weaponModel.ZOffset);
+        spawnedWeapon.transform.localRotation = Quaternion.Euler(0,0,0);
+    }
+}
+
